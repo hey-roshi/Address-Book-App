05f814a8970e1413374f27632659f48c7a1ab526
roshi <srivastavaroshi00@gmail.com>
2025-03-12 01:54:11 +0530

    introduce DTO and Model

---
+++ src/main/java/com/example/AddressBookApp/dto/ContactDTO.java
@@ -0,0 +1,25 @@
+package com.example.AddressBookApp.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ContactDTO {
+
+    @NotBlank(message = "First name is mandatory")
+    private String firstName;
+
+    @NotBlank(message = "Last name is mandatory")
+    private String lastName;
+
+    @Email(message = "Invalid email")
+    private String email;
+
+    @NotBlank(message = "Phone is mandatory")
+    private String phone;
+}
